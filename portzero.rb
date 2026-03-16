08f50031574169cabec63dc1d8753a1ddb8fab2a
David Viejo <dviejo@kungfusoftware.es>
2026-03-16 23:00:00 +0100

    chore: bump portzero to 0.2.1

--- portzero.rb
+++ portzero.rb
@@ -1,28 +1,28 @@
 class Portzero < Formula
   desc "Local development reverse proxy, process manager, and traffic inspector"
   homepage "https://goport0.dev"
-  version "0.2.0"
+  version "0.2.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
-      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.0/portzero-darwin-aarch64.tar.gz"
-      sha256 "5c618b21d8ef4b68bf446d9bd89b63a0eb88d1e1280dc28d33f549bfe36d82dd"
+      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.1/portzero-darwin-aarch64.tar.gz"
+      sha256 "49e1c9c9bcbc30aa0c023d96178abed5603ae4da6b349c9dc84cb95f680165fe"
     end
     on_intel do
-      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.0/portzero-darwin-x86_64.tar.gz"
-      sha256 "6a20b25cfe4f718826f0bc23ca3899413e9c7cc3519a3faf4cc71f2f0426060c"
+      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.1/portzero-darwin-x86_64.tar.gz"
+      sha256 "2de28cc212124372ff088bc7bab9c6738f7d617b7a330042bb21a3f13a4116ff"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.0/portzero-linux-aarch64.tar.gz"
-      sha256 "adeeff3ecce11608ddf46751822373fa03461f930511cc89eaa704adc5d32d88"
+      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.1/portzero-linux-aarch64.tar.gz"
+      sha256 "6e26a29ae72e5a3e5e7e958b6f6287615c1dd80a398ab2edc7633a42c9a0aba9"
     end
     on_intel do
-      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.0/portzero-linux-x86_64.tar.gz"
-      sha256 "0b6225452be89093fe48c0009899c86e332629c4c6d562ca98a144ac10d913ce"
+      url "https://github.com/portzero-dev/portzero/releases/download/cli-v0.2.1/portzero-linux-x86_64.tar.gz"
+      sha256 "7aca22af5639652857a9086ec6d4a6fdb3ff733bc45869e4cd052240233f2fe4"
     end
   end
 
